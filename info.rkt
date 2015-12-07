2b41d7752b673b7ebd1ad120cd80607de32f89de
ben <types@ccs.neu.edu>
2015-12-06 23:22:40 -0500

    ** remove cover from build-deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "dependent")
 (define deps '("base"))
-(define build-deps '("cover" "cover-coveralls" "scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define pkg-desc "Strongly-typed macros")
 (define version "0.0")
 (define pkg-authors '(ben))
