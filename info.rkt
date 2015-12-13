9cac53695b083397a62bb69bb78ff35fad82da94
ben <types@ccs.neu.edu>
2015-12-12 22:22:19 -0500

    ** change package name

--- info.rkt
+++ info.rkt
@@ -1,8 +1,9 @@
 #lang info
-(define collection "dependent")
+
+(define collection 'use-pkg-name)
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define pkg-desc "Strongly-typed macros")
 (define version "0.0")
 (define pkg-authors '(ben))
-(define scribblings '(("scribblings/dependent.scrbl")))
+(define scribblings '(("scribblings/trivial.scrbl")))
