876e49ff9392a90b31a47fffd25a73860ea5c01f
ben <types@ccs.neu.edu>
2015-12-14 03:05:04 -0500

    ** add main.rkt

---
+++ main.rkt
@@ -0,0 +1,11 @@
+#lang typed/racket/base
+
+(provide
+  (all-from-out trivial/format)
+  (all-from-out trivial/math)
+  (all-from-out trivial/regexp))
+
+(require
+  trivial/format
+  trivial/math
+  trivial/regexp)
