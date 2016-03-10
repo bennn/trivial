81f4de4c105a0197647dd7712f9fdcedd084a199
ben <benjaminlgreenman@gmail.com>
2016-03-10 00:07:21 -0500

    ** update pkg deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,18 @@
 #lang info
 (define collection 'use-pkg-name)
-(define deps '("base" "typed-racket-lib" "typed-racket-more"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define deps '("base"
+               "typed-racket-lib"
+               "typed-racket-more"
+               "db-lib"
+               "math-lib"
+               "plot-lib"
+               "rackunit-lib"
+               "scribble-lib"))
+(define build-deps '("scribble-lib"
+                     "at-exp-lib"
+                     "racket-doc"
+                     "rackunit-abbrevs"
+                     "rackunit-lib"))
 (define pkg-desc "Strongly-typed macros")
 (define version "0.1")
 (define pkg-authors '(ben))
